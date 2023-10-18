52fdc59b6bef6f7a2cb4dce5e8760049bbdca9fd
azizul96 <alifalamin42@gmail.com>
2023-10-19 01:14:45 +0600

    implement post and get method to create and read data

--- index.js
+++ index.js
@@ -29,6 +29,19 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const productCollection = client.db("brandShopDB").collection("products");
+
+    app.post('/products', async(req, res)=>{
+      const product = req.body
+      const result = await productCollection.insertOne(product)
+      res.send(result)
+
+    })
+    app.get('/products', async(req, res)=>{
+      const result = await productCollection.find().toArray()
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
