601de063c363b8422890ffaa8042a91acca120ef
azizul96 <alifalamin42@gmail.com>
2023-10-19 09:43:37 +0600

    Apply delete method to delete carts on my cart page

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
     await client.connect();
 
     const productCollection = client.db("brandShopDB").collection("products");
+    const cartCollection = client.db("allCartDB").collection("carts");
 
     app.post('/products', async(req, res)=>{
       const product = req.body
@@ -68,6 +69,24 @@ async function run() {
       res.send(result)
 
   })
+  // cart operation
+  app.post('/carts', async(req, res)=>{
+    const cart = req.body
+    const result = await cartCollection.insertOne(cart)
+    res.send(result)
+
+  })
+  app.get('/carts', async(req, res)=>{
+    const result = await cartCollection.find().toArray()
+    res.send(result)
+  })
+  app.delete('/carts/:id', async(req, res)=>{
+    const id = req.params.id
+    const query = { _id: new ObjectId(id) }
+    const result = await cartCollection.deleteOne(query)
+    res.send(result)
+  })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
